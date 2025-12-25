d3c6751a7fb824c4c7fb19084a212a87747608a9
lavanyamadaka54 <lavanyamadaka54@gmail.com>
2025-12-25 15:27:54 +0530

    episode-2

---
+++ episode-2/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const heading = React.createElement("h1",{id:"heading"},"Hello React");
+
+
+const mainContainer = React.createElement("div", {}, [
+    React.createElement("div", {}, [
+        React.createElement("h1", {}, "I am the heading 1"),
+        React.createElement("h2", {}, "I am the heading 2")
+    ]),
+    React.createElement("div", {}, [
+        React.createElement("h1", {}, "I am the heading 1"),
+        React.createElement("h2", {}, "I am the heading 2")
+    ])
+])
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(mainContainer)
