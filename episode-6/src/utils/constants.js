4fb21342e3f09e391655ad80ada831574c49a49e
lavanyamadaka54 <lavanyamadaka54@gmail.com>
2026-01-03 21:48:08 +0530

    episode-6

---
+++ episode-6/src/utils/constants.js
@@ -0,0 +1,4 @@
+export const LOGO_URL =
+  "https://static.vecteezy.com/system/resources/thumbnails/055/349/890/small/chef-mascot-logo-design-for-restaurant-cooking-man-tasty-express-food-illustration-vector.jpg";
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/f_auto,q_auto";
