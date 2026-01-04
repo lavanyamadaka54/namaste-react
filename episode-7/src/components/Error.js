b92f7d110aa2a0c8ba7d45e57692d3de3caad7c8
lavanyamadaka54 <lavanyamadaka54@gmail.com>
2026-01-04 15:34:29 +0530

    episode-7

---
+++ episode-7/src/components/Error.js
@@ -0,0 +1,14 @@
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+  const error = useRouteError();
+  
+  return (
+    <div>
+      <h1>Oops! Something went wrong</h1>
+      <h1>{`${error.status}: ${error.statusText}`}</h1>
+    </div>
+  );
+};
+
+export default Error
